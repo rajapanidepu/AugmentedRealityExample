d9d2ce59edce3a6d96398e8584ae0d585739695f
Pedro Gomez <pgomez@tuenti.com>
2013-05-01 13:39:15 +0200

    Add VuforiaLibrary for augmented reality projects

---
+++ AugmentedRealityExample/Assets/Qualcomm Augmented Reality/Scripts/MultiTarget.cs
@@ -0,0 +1,14 @@
+﻿/*==============================================================================
+Copyright (c) 2010-2013 QUALCOMM Austria Research Center GmbH.
+All Rights Reserved.
+Qualcomm Confidential and Proprietary
+==============================================================================*/
+
+/// <summary>
+/// A trackable that is made up of multiple targets with a fixed spatial
+/// relation.
+/// </summary>
+public interface MultiTarget : Trackable
+{
+
+}
---
+++ AugmentedRealityExample/Assets/Qualcomm Augmented Reality/Scripts/TrackableSource.cs
@@ -0,0 +1,13 @@
+﻿/*==============================================================================
+Copyright (c) 2010-2013 QUALCOMM Austria Research Center GmbH.
+All Rights Reserved.
+Qualcomm Confidential and Proprietary
+==============================================================================*/
+
+/// <summary>
+/// An opaque handle for creating a new Trackable in a DataSet.
+/// </summary>
+public abstract class TrackableSource
+{
+         
+}
